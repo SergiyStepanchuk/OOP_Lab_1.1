d49fba4989e7e68896eb99689349eed3be3ed63a
SergiyStepanchuk <prochliste@gmail.com>
2021-02-22 12:22:33 +0200

    init

---
+++ OOP_Lab_1.1/Program/CProgression.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <cmath>
+
+class Progression
+{
+	float _first = 0;
+	float _second = 0;
+
+public:
+
+	bool Init(const float &first, const float &second);
+	void Read();
+	void Display() const;
+
+	float	GetFirst()	const;
+	float	GetSecond() const;
+
+	void	SetFirst(const float& val);
+	void	SetSecond(const float& val);
+
+	float	elementJ(const unsigned int& j) const;
+};
+
---
+++ OOP_Lab_1.1/UnitTest/UnitTest.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Program/CProgression.h"
+#include "../Program/CProgression.cpp"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(TestProgression)
+	{
+	public:
+		
+		TEST_METHOD(Init)
+		{
+			Progression pgr;
+			Assert::AreEqual(pgr.Init(1, 0), false);
+		}
+	};
+}
